b317c3b54426d9bc4947fff49336f91b7b54e45a
kalitawojciech <wojtek1311997@gmail.com>
2019-03-18 12:41:49 +0100

    added new empty file for new component and for file with some functions

--- MyFirstApp/src/components/EventList.js
+++ MyFirstApp/src/components/EventList.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
-import { Text } from 'react-native';
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable class-methods-use-this */
 
+import React, { Component } from 'react';
+import { FlatList, Text } from 'react-native';
 class EventList extends Component {
+    state={
+        events: []
+    }
+
+    componentDidMount() {
+        const events = require('../../db.json').events;
+        this.setState({ events });
+    }
+
     render () {
         return (
-            <Text>Hello World!</Text>
+            <FlatList 
+            data={this.state.events}
+            renderItem={({ item }) => <Text>{item.title}</Text>}
+            keyExtractor={item => item.id}
+            />
         );
     }
 }
